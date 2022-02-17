2d9e4d80a503b1f1273d9da89ee7e7b667aafd22
Steve Shanks <steve.c.shanks@gmail.com>
2022-02-17 21:01:09 +0000

    Move message validation to struct

--- stock-notifier/consumer/consumer.go
+++ stock-notifier/consumer/consumer.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 
 	"github.com/segmentio/kafka-go"
 	"github.com/stevecshanks/db-updates-to-domain-events.git/stock-notifier/stock"
@@ -21,6 +22,17 @@ type updatePayload struct {
 
 type updateMessage struct {
 	Payload updatePayload `json:"payload"`
+}
+
+func (um updateMessage) Validate() error {
+	if um.Payload.Before == nil && um.Payload.After == nil {
+		return errors.New("payload is empty")
+	}
+	if um.Payload.Before != nil && um.Payload.After != nil && um.Payload.Before.ProductID != um.Payload.After.ProductID {
+		return errors.New("product ids do not match")
+	}
+
+	return nil
 }
 
 type kafkaReader interface {
@@ -48,26 +60,19 @@ func (c consumer) ReadUpdate(ctx context.Context) (*stock.Update, error) {
 		return nil, err
 	}
 
-	update, err := createUpdate(message)
+	err = message.Validate()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("invalid message: %w", err)
 	}
 
-	return update, nil
+	return createUpdate(message), nil
 }
 
 func isTombstone(kafkaMessage kafka.Message) bool {
 	return len(kafkaMessage.Value) == 0
 }
 
-func createUpdate(message updateMessage) (*stock.Update, error) {
-	if message.Payload.Before == nil && message.Payload.After == nil {
-		return nil, errors.New("invalid message: payload is empty")
-	}
-	if message.Payload.Before != nil && message.Payload.After != nil && message.Payload.Before.ProductID != message.Payload.After.ProductID {
-		return nil, errors.New("invalid message: product ids do not match")
-	}
-
+func createUpdate(message updateMessage) *stock.Update {
 	update := stock.Update{}
 	if message.Payload.Before != nil {
 		update.ProductID = message.Payload.Before.ProductID
@@ -78,7 +83,7 @@ func createUpdate(message updateMessage) (*stock.Update, error) {
 		update.NewQuantity = &message.Payload.After.Quantity
 	}
 
-	return &update, nil
+	return &update
 }
 
 // New creates a new consumer using the provided Kafka reader
